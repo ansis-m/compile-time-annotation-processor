4fd113fdb3ad04bebaff0b8faaefe84ffb5710fb
ansis@home <ansis@home>
2024-08-11 13:39:16 +0300

    init multimodule project

---
+++ annotation-processor/src/main/java/com/example/processor/AnnotationProcessor.java
@@ -0,0 +1,12 @@
+package com.example.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class AnnotationProcessor {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ annotation-processor/src/test/java/com/example/processor/AnnotationProcessorTest.java
@@ -0,0 +1,11 @@
+package com.example.processor;
+
+import org.junit.jupiter.api.Test;
+
+class AnnotationProcessor {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
